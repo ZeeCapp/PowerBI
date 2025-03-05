d1a63e964e3b3ace39087f557333f104c76148a9
rstefko <roman.stefko@eway-crm.com>
2025-03-05 13:07:17 +0100

    refs #48894: Added settings.json, fixed publish script

--- local_publish.bat
+++ local_publish.bat
@@ -1,2 +1,4 @@
-copy %~dp0bin\Release\eWay_CRM.mez "%USERPROFILE%\Documents\Power BI Desktop\Custom Connectors"
-copy %~dp0bin\Release\eWay_CRM.mez "C:\Windows\ServiceProfiles\PBIEgwService\Documents\Power BI Desktop\Custom Connectors"
+IF NOT EXIST "%USERPROFILE%\Documents\Power BI Desktop\Custom Connectors" MKDIR "%USERPROFILE%\Documents\Power BI Desktop\Custom Connectors"
+COPY %~dp0bin\Debug\eWay_CRM.mez "%USERPROFILE%\Documents\Power BI Desktop\Custom Connectors"
+COPY %~dp0bin\Debug\eWay_CRM.mez "C:\Windows\ServiceProfiles\PBIEgwService\Documents\Power BI Desktop\Custom Connectors"
+PAUSE
